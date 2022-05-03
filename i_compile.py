3eb9ab48bcea4f1642f18cf26e85919dac8bd5f8
yyc001 <1594088331@qq.com>
2022-05-03 17:27:48 +0800

    dbg

--- i_compile.py
+++ i_compile.py
@@ -15,7 +15,7 @@ def isNum(s):
         return False
 
 
-with open("source.s", encoding="utf-8") as f:
+with open("test.s", encoding="utf-8") as f:
     line_id = 0
     bin_ad = 0
     for line in f.readlines():
@@ -58,7 +58,7 @@ with open("source.s", encoding="utf-8") as f:
         })
         bin_ad += 1 + imme
 
-with open("source.mif", "w") as f:
+with open("test.mif", "w") as f:
     f.write("""WIDTH=16;
 DEPTH=256;
 
